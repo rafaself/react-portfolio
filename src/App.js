a35b9907421d4e3541ee4e23e1c3451f3e28183d
rafael <rafaondjango@gmail.com>
2024-02-04 20:46:39 +0000

    test mobile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import MyList from "./components/MyList";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import { ImageList, ImageListItem } from "@mui/material";
 
 function App() {
 
@@ -14,8 +15,8 @@ function App() {
       <div className="app">
         <Grid container spacing={2}>
 
-          <Grid item md={6} sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>  
-            <img src="images/profile2.png" alt="logo" className="frameworks" height="450"/>
+          <Grid item md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <img src="images/profile2.png" alt="logo" className="frameworks" height="450" />
             <span>Está página foi desenvolvida utilizando React</span>
           </Grid>
 
@@ -27,9 +28,16 @@ function App() {
             <AboutMe />
           </Grid>
 
-          <Grid item md={6} sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>  
-            <img src="images/git-rocket.svg" alt="logo"/>
+          <Grid item md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", overflow: "hidden" }} >
+            {/* <img src="images/git-rocket.svg" alt="logo" /> */}
+            <ImageList sx={{ width: 500, height: 300 }} cols={1} rowHeight={"auto"}>
+
+              <ImageListItem>
+                <img src="images/git-rocket.svg" alt="logo" />
+              </ImageListItem>
+            </ImageList>
           </Grid>
+
 
           {/*
 
