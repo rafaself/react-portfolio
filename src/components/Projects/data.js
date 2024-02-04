5d9c59b9a1b591984566ba3be1e6ebfabfc52a8b
rafael <rafaondjango@gmail.com>
2024-02-04 21:18:26 +0000

    adicionando projetos

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,5 +1,12 @@
 export const cardsInfo = [
     {
+        title: "Gerrenciamento de usuários com FastAPI",
+        description: "Software que realiza o gerenciamento usuários e suas permissões e autenticações.",
+        image: "https://dkrn4sk0rn31v.cloudfront.net/uploads/2022/03/o-que-e-fastapi-400x280.png",
+        imageTitle: "fast api projeto",
+        url: "https://github.com/rafaself/python-fastapi-user-management"
+    },
+    {
         title: "Regex (scripts)",
         description: "Scripts para realizar verificações e validações de padrões.",
         image: "/images/regex.png",
