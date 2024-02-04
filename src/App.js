76b65dc3f64b3c5de5806d5291dbfd46646b2517
rafael <rafaondjango@gmail.com>
2024-02-04 15:56:02 +0000

    estilizando a pagina

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import MyList from "./components/MyList";
 
 function App() {
   return (
-    <div>
+    <div class="main">
       <div className="space">
       </div>
 
       <div className="app">
-        <Grid container spacing={2}>
+        <Grid container spacing={2} paddingX={8}>
 
           <Grid item md={6} display="flex" justifyContent="center">
             <img src="images/profile2.png" alt="logo" class="frameworks" />
           </Grid>
 
-          <Grid item md={6} container justifyContent="center">
+          <Grid item md={6} alignItems="center">
+
             <Typography variant="h4">
               Rafael Moraes
             </Typography>
+
             <Typography mt={2} variant="h6">
               Bem vindo(a) a minha página! Sou desenvolvedor backend com foco em Python.
             </Typography>
+            
             <MyList />
+
           </Grid>
 
           <Grid item md={12}>
--- src/components/MyList/index.js
+++ src/components/MyList/index.js
@@ -7,42 +7,156 @@ import Avatar from '@mui/material/Avatar';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import MailIcon from '@mui/icons-material/Mail';
-import { Divider } from '@mui/material';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Divider, ListItemButton, Typography } from '@mui/material';
 
 export default function MyList() {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'transparent' }}>
+    <List sx={{ width: '100%', maxWidth: "100%", bgcolor: 'transparent' }}>
 
       <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <LinkedInIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        <Typography variant="h7">
+          Minhas redes:
+        </Typography>
       </ListItem>
 
-      <Divider />
+      <Divider sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3,
+        marginY: 1
+      }}/>
 
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <MailIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
+      <ListItem sx={{
+        bgcolor: () => 'rgba(0, 119, 181, 0.1)',
+        borderRadius: 3
+      }}>
+
+        <ListItemButton component="a" href="https://www.linkedin.com/in/rafael-fontenele/" target="_blank">
+
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: "#cce4f6" }}>
+              <LinkedInIcon sx={{ color: "#0077B5" }} />
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText
+            primary="Linkedin"
+            secondary="https://www.linkedin.com/in/rafael-fontenele/"
+            secondaryTypographyProps={{
+              color: 'FFFFFF',
+              fontWeight: 'medium',
+              variant: 'body2',
+            }}
+          />
+
+        </ListItemButton>
+
       </ListItem>
 
-      <Divider />
+      <Divider sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3,
+        marginY: 1
+      }}/>
 
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <GitHubIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
+      <ListItem sx={{
+        bgcolor: () => 'rgba(255, 255, 255, 0.1)',
+        borderRadius: 3
+      }}>
+
+        <ListItemButton component="a" href="https://github.com/rafaself" target="_blank">
+
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: "#FFFFFF" }}>
+              <GitHubIcon sx={{ color: "#000000" }} />
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText
+            primary="GitHub"
+            secondary="https://github.com/rafaself"
+            secondaryTypographyProps={{
+              color: 'FFFFFF',
+              fontWeight: 'medium',
+              variant: 'body2',
+            }}
+          />
+
+        </ListItemButton>
+
       </ListItem>
+
+      <Divider sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3,
+        marginY: 0.5
+      }}/>
+
+      <ListItem sx={{
+        bgcolor: () => 'rgba(37, 211, 102, 0.1)',
+        borderRadius: 3
+      }}>
+
+        <ListItemButton component="a" href="https://wa.me/63981189986" target="_blank">
+
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: "#EEEEEE" }}>
+              <WhatsAppIcon sx={{ color: "#25D366" }} />
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText
+            primary="WhatsApp"
+            secondary="(63) 9 8118-9986"
+            secondaryTypographyProps={{
+              color: 'FFFFFF',
+              fontWeight: 'medium',
+              variant: 'body2',
+            }}
+          />
+
+        </ListItemButton>
+
+      </ListItem>
+
+      <Divider sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3,
+        marginY: 1
+      }}/>
+
+      <ListItem sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3
+      }}>
+
+        <ListItemButton component="a" href="mailto:rafaondjango@gmail.com" target="_blank">
+
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: "#EEEEEE" }}>
+              <MailIcon sx={{ color: "#FF0000" }} />
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText
+            primary="Email"
+            secondary="rafaondjango@gmail.com"
+            secondaryTypographyProps={{
+              color: '#FFFFFF',
+              fontWeight: 'medium',
+              variant: 'body2',
+            }}
+          />
+
+        </ListItemButton>
+
+      </ListItem>
+
+      <Divider sx={{
+        bgcolor: () => 'rgba(255, 0, 0, 0.1)',
+        borderRadius: 3,
+        marginY: 1
+      }}/>
 
     </List>
   );
