2b08ff95606db5c830a9da8777fe5f2adb3a8778
rafael <rafaondjango@gmail.com>
2024-02-10 19:46:04 +0000

    organizando projetos

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,20 +1,34 @@
 export const cardsInfo = [
     {
-        title: "Python Gerrenciamento de usuários com FastAPI",
+        title: "Python FastAPI - API para gerenciamento de usuários",
         description: "Software que realiza o gerenciamento de usuários, permissões e autenticações. Utiliza Python, FastAPI, SQLAlchemy, RabbitMQ, Celery, Docker e Pydantic.",
         image: "https://dkrn4sk0rn31v.cloudfront.net/uploads/2022/03/o-que-e-fastapi-400x280.png",
         imageTitle: "fast api projeto",
         url: "https://github.com/rafaself/python-fastapi-user-management"
     },
     {
-        title: "C# ASP.NET Api Genérica",
+        title: "C# ASP.NET - API genérica",
         description: "Software desenvolvida utilizando C# ASP.NET e Entity para gerenciar os recursos de uma API.",
         image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg",
         imageTitle: "c# asp dot net",
         url: "https://github.com/rafaself/c--asp.net-cinemaclub"
     },
     {
-        title: "SpaceApp (Em andamento)",
+        title: "C# ASP.NET - API para gerenciamento de usuários",
+        description: "Software desenvolvida utilizando C# ASP.NET e Entity para gerenciar os recursos de uma API.",
+        image: "https://growiz.com.br/wp-content/uploads/2021/01/ms-dotnet-3-1024x1024.png",
+        imageTitle: "c# asp dot net",
+        url: "https://github.com/rafaself/CS-.NET-user-management"
+    },
+    {
+        title: "GO Gin - API genérica",
+        description: "Desenvolvimento de uma API REST em Go com o framework Gin Rest.",
+        image: "https://hermes.dio.me/articles/cover/93400613-9e4c-44c9-b42a-9a2972af3060.png",
+        imageTitle: "go api gin rest",
+        url: "https://github.com/rafaself/go-gin-apirest",
+    },
+    {
+        title: "React - SpaceApp (Em andamento)",
         description: "Construção de Frontend utilizando React, Vite, Typescript, Gerenciamento de Estados, React Router, Tests, Mocks e Styled Components.",
         image: "/images/spaceapp.png",
         imageTitle: "space app",
@@ -22,21 +36,14 @@ export const cardsInfo = [
         urlResult: "https://react-spaceapp.vercel.app/",
     },
     {
-        title: "Golang - Gin",
-        description: "Desenvolvimento de uma API REST em Go com o framework Gin Rest.",
-        image: "https://hermes.dio.me/articles/cover/93400613-9e4c-44c9-b42a-9a2972af3060.png",
-        imageTitle: "go api gin rest",
-        url: "https://github.com/rafaself/go-gin-apirest",
-    },
-    {
-        title: "Regex (scripts)",
+        title: "Python Scripts - Regex",
         description: "Scripts para realizar verificações e validações de padrões.",
         image: "/images/regex.png",
         imageTitle: "regex",
         url: "https://github.com/rafaself/python-regex"
     },
     {
-        title: "Senhor dos anéis",
+        title: "React - Senhor dos anéis",
         description: "Frontend com o tema do Senhor dos anéis desenvolvido para estudos com React.",
         image: "/images/lotr.png",
         imageTitle: "senhor dos anéis",
@@ -44,7 +51,7 @@ export const cardsInfo = [
         urlResult: "https://lotr-react-seven.vercel.app/",
     },
     {
-        title: "Site de Engenharia",
+        title: "Vue - Site de Engenharia",
         description: "Frontend desenvolvido com Vuejs para site de Engenharia Elétrica.",
         image: "https://belenos.me/media/2021-05-vuejs.webp",
         imageTitle: "logo ares engenharia",
@@ -52,14 +59,14 @@ export const cardsInfo = [
         urlResult: "https://vuejs-ares-frontend.vercel.app/",
     },
     {
-        title: "Perceptron",
+        title: "Python - Perceptron",
         description: "Implementação do Perceptron para implementação da porta AND. Conteúdo básico de Redes Neurais Artificais.",
         image: "https://matt.might.net/images/blog/midjourney-glowing-blue-neural-network-small.png",
         imageTitle: "redes neurais representação",
         url: "https://github.com/rafaself/python-perceptron-basics",
     },
     {
-        title: "HTML e CSS puro",
+        title: "HTML e CSS - Página básica",
         description: "Página básica com HTML e CSS.",
         image: "https://horadecodar.com.br/wp-content/uploads/2019/06/html_css_oinicio.png",
         imageTitle: "html e css",
@@ -67,14 +74,14 @@ export const cardsInfo = [
         urlResult: "https://htmlcss-basic-page.vercel.app/",
     },
     {
-        title: "Pyautogui (scripts)",
+        title: "Python Scripts - Pyautogui",
         description: "Scripts para realizar tarefas com com pyautogui.",
         image: "https://rightcode.co.jp/wp-content/uploads/2020/04/pyautogui-logo-001.png",
         imageTitle: "pyautogui",
         url: "https://github.com/rafaself/python-pyautogui-scripts",
     },
     {
-        title: "Java UFC Game",
+        title: "Java - UFC Game",
         description: "Jogo básico para visualizar no terminal iterações entre de lutadores com Java.",
         image: "https://i.redd.it/oog08qvto2k91.jpg",
         imageTitle: "logo ufc",
